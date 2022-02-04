4a5e6bd712eaab0fb279a6d4868943188b83a84f
Libhut Samwes <libhut-samwes@protonmail.com>
2022-02-04 15:57:31 +0800

    add components for navbar gameboard and footer

---
+++ src/components/GameBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './GameBoard.css'
+
+class GameBoard extends Component {
+	render() {
+		return (
+			<div>
+				<p>Gameboard</p>
+			</div>
+		);
+	}
+}
+
+export default GameBoard;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+	render() {
+		return (
+			<div>
+				<p>NavBar</p>
+			</div>
+		);
+	}
+}
+
+export default NavBar;
